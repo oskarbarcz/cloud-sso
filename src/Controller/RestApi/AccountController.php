af406c24fa5dbf0ac5bf32e95562f1597f7c3beb
Oskar Barcz <kontakt@archi-tektur.pl>
2020-05-02 22:20:30 +0200

    Add required SSO endpoints

--- src/Controller/RestApi/AccountController.php
+++ src/Controller/RestApi/AccountController.php
@@ -7,14 +7,18 @@ namespace App\Controller\RestApi;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class AccountController extends AbstractController
 {
     /**
      * @Route("/api/account/current", name="api_account_current")
+     * @param SerializerInterface $serializer
+     * @return JsonResponse
      */
-    public function getOne()
+    public function getOne(SerializerInterface $serializer): JsonResponse
     {
-        return new JsonResponse('Acc');
+        $content = $serializer->serialize($this->getUser(), 'json');
+        return new JsonResponse($content, 200, [], true);
     }
 }
