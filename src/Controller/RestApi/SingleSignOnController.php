a4384863b6123623b1b29cd907d6f8be6b39b2d7
Oskar Barcz <kontakt@archi-tektur.pl>
2020-05-02 23:45:19 +0200

    Add all required basecode for starting over authing

--- src/Controller/RestApi/SingleSignOnController.php
+++ src/Controller/RestApi/SingleSignOnController.php
@@ -4,14 +4,37 @@ declare(strict_types=1);
 
 namespace App\Controller\RestApi;
 
+use App\Repository\AccountRepository;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class SingleSignOnController extends AbstractController
 {
-    public function handleLogin()
+    private JWTTokenManagerInterface $manager;
+    private AccountRepository $repository;
+
+    public function __construct(JWTTokenManagerInterface $manager, AccountRepository $repository)
+    {
+        $this->manager = $manager;
+        $this->repository = $repository;
+    }
+
+    /**
+     * @Route("/sso-login", name="sso_login")
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function handleLogin(Request $request): RedirectResponse
     {
         // check if server is allowed
         // check if user is logged in (HOW???)
         //
+        $account = $this->repository->findOneBy(['email' => 'test0@example.com']);
+
+        $token = $this->manager->create($account);
+        return new RedirectResponse("http://localhost:8080/sso-success/{$token}");
     }
 }
