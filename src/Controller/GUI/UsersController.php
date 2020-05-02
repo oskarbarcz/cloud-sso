8d30b936c2b1c9ad09f1387291f9f4471aa534f0
Oskar Barcz <kontakt@archi-tektur.pl>
2020-05-02 14:49:36 +0200

    Add working login mechanism

--- src/Controller/GUI/UsersController.php
+++ src/Controller/GUI/UsersController.php
@@ -4,15 +4,16 @@ declare(strict_types=1);
 
 namespace App\Controller\GUI;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class UsersController
+class UsersController extends AbstractController
 {
     /**
-     * @Route("/", name="dashboard")
+     * @Route("/", name="app_dashboard")
      */
     public function dashboard()
     {
-
+        return $this->render('dashboard.html.twig');
     }
 }
