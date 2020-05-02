9d7a4a18039890da7bc17e7ccb64cec97c9ccccf
Oskar Barcz <kontakt@archi-tektur.pl>
2020-05-02 21:19:20 +0200

    Add the user component

--- src/Controller/GUI/UsersController.php
+++ src/Controller/GUI/UsersController.php
@@ -4,16 +4,24 @@ declare(strict_types=1);
 
 namespace App\Controller\GUI;
 
+use App\Entity\Account;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UsersController extends AbstractController
 {
     /**
+     * Renders current user screen
+     *
      * @Route("/", name="app_dashboard")
      */
-    public function dashboard()
+    public function dashboard(): Response
     {
-        return $this->render('dashboard.html.twig');
+        if (!$this->getUser() instanceof Account) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('pages/dashboard.html.twig');
     }
 }
