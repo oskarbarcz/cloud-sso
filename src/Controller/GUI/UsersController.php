059a2ddfe1355d8b112276bcbacf62cdb69b0286
Oskar Barcz <kontakt@archi-tektur.pl>
2020-05-02 22:33:01 +0200

    Masterize security

--- src/Controller/GUI/UsersController.php
+++ src/Controller/GUI/UsersController.php
@@ -4,24 +4,28 @@ declare(strict_types=1);
 
 namespace App\Controller\GUI;
 
-use App\Entity\Account;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UsersController extends AbstractController
 {
     /**
+     * @Route("/", name="index")
+     */
+    public function index(): RedirectResponse
+    {
+        return $this->redirectToRoute('app_dashboard');
+    }
+
+    /**
      * Renders current user screen
      *
-     * @Route("/", name="app_dashboard")
+     * @Route("/dashboard", name="app_dashboard")
      */
     public function dashboard(): Response
     {
-        if (!$this->getUser() instanceof Account) {
-            return $this->redirectToRoute('app_login');
-        }
-
         return $this->render('pages/dashboard.html.twig');
     }
 }
